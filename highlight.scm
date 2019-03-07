383b83d25e952fb6b324b386e54b272a099ebc1c
Olof-Joachim Frahm (欧雅福) <olof@macrolet.net>
2019-03-07 01:11:49 +0100

    Add rounded border option.

--- highlight.scm
+++ highlight.scm
@@ -1,8 +1,9 @@
-(define (script-fu-highlight image drawable shadow-color shadow-opacity use-layer-opacity feather-border)
+(define (script-fu-highlight image drawable shadow-color shadow-opacity use-layer-opacity feather-border round-border)
   (if (= (car (gimp-selection-is-empty image)) FALSE)
       (let ((image-width (car (gimp-image-width image)))
             (image-height (car (gimp-image-height image)))
-            (type (car (gimp-drawable-type-with-alpha drawable))))
+            (type (car (gimp-drawable-type-with-alpha drawable)))
+            (save-selection (or (> feather-border 0) (> round-border 0))))
         (gimp-context-push)
         (gimp-context-set-defaults)
         (gimp-image-undo-group-start image)
@@ -11,17 +12,20 @@
           (gimp-image-insert-layer image highlight-layer 0 -1)
 
           (let ((selection 0))
-            (if (> feather-border 0)
+            (if save-selection
                 (begin
                   (set! selection (car (gimp-selection-save image)))
-                  (gimp-selection-feather image feather-border)))
+                  (if (> round-border 0)
+                      (script-fu-selection-rounded-rectangle image drawable round-border FALSE))
+                  (if (> feather-border 0)
+                      (gimp-selection-feather image feather-border))))
 
             (gimp-selection-invert image)
 
             (gimp-context-set-background shadow-color)
             (gimp-edit-bucket-fill highlight-layer BG-BUCKET-FILL NORMAL-MODE (if (= use-layer-opacity TRUE) 100 shadow-opacity) 0 FALSE 0 0)
 
-            (if (> feather-border 0)
+            (if save-selection
                 (begin
                   (gimp-image-select-item image CHANNEL-OP-REPLACE selection)
                   (gimp-image-remove-channel image selection))
@@ -38,13 +42,14 @@
   "Olof-Joachim Frahm <olof@macrolet.net>"
   "Olof-Joachim Frahm"
   "2019"
-  ""
+  "RGB* GRAY*"
   SF-IMAGE      "Image"              0
   SF-DRAWABLE   "Drawable"           0
   SF-COLOR      _"Color"             "black"
   SF-ADJUSTMENT _"Opacity"           '(50 0 100 1 10 0 0)
   SF-TOGGLE     _"Use Layer Opacity" FALSE
-  SF-ADJUSTMENT _"Feather Border"    '(0 0 50 1 5 0 0))
+  SF-ADJUSTMENT _"Feather Border"    '(0 0 50 1 5 0 0)
+  SF-ADJUSTMENT _"Round Border (%)"    '(0 0 100 1 5 0 0))
 
 (script-fu-menu-register "script-fu-highlight"
-                         "<Image>/Filters/Decor")
+                         "<Image>/Filters/Light and Shadow/Shadow")
